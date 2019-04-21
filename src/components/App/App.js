f7b3407afbb423700494e2f08a4bc57dffd86a8e
fancn <fanyj@yikeer.com>
2019-04-21 22:43:11 +0800

    redux form intro

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import AntdForm from "../AntdForm";
+import ReduxForm from "../ReduxForm";
 import "./App.css";
 
 function Index() {
   return <h2>This is HOME Page</h2>;
 }
 
-function AppRouter() {
-  return (
+const App = ({ store }) => (
+  <Provider store={store}>
     <Router>
       <div className="demo">
         <nav className="demo-nav">
@@ -28,10 +31,15 @@ function AppRouter() {
         <div className="demo-body">
           <Route path="/" exact component={Index} />
           <Route path="/antd-form/" component={AntdForm} />
+          <Route path="/redux-form/" component={ReduxForm} />
         </div>
       </div>
     </Router>
-  );
-}
+  </Provider>
+);
 
-export default AppRouter;
+App.propTypes = {
+  store: PropTypes.object.isRequired
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
+import configureStore from "./configureStore";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App store={configureStore()} />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
